323f587a2a9e54243c074dc351571f4aab835ecf
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-09 23:34:35 +0200

    Remove unnecessary code

--- Resource_Class.py
+++ Resource_Class.py
@@ -1,14 +1,14 @@
 from enum import Enum
+
 class ResourceType(Enum):
     Metal = 1
     Crystal = 2
     Deuter = 3
 
 class Resource:
-
     def __init__(self, resourceType):
         self.resourceType = resourceType
-        self.quantity = int(0)
+        self.quantity = 0
 
     def getType(self):
         return self.resourceType
@@ -16,27 +16,11 @@ class Resource:
     def getQuantity(self):
         return self.quantity
 
-    def setQuantity(self,value):
+    def setQuantity(self, value):
         self.quantity = value
 
-    def add(self,otherResource):
-        self.quantity = self.quantity + otherResource.getQuantity()
+    def add(self, otherResource):
+        self.quantity += otherResource.getQuantity()
 
-    def substract(self,otherResource):
-        self.quantity = self.quantity - otherResource.getQuantity()
-
-
-
-
-metal = Resource(ResourceType.Metal)
-crystal = Resource(ResourceType.Crystal)
-metal.setQuantity(8)
-crystal.setQuantity(200)
-name = metal.getQuantity()
-print (name)
-metal.add(crystal)
-name = metal.getQuantity()
-print(name)
-metal.substract(crystal)
-name = metal.getQuantity()
-print (name)
+    def substract(self, otherResource):
+        self.quantity -= otherResource.getQuantity()
