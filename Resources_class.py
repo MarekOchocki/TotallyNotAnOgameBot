39acbc6eb0b210b449aa9f0d7648a7004ad4d70a
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-10 21:08:53 +0200

    fix method names ,add connect with Resource_Class file and change type's of fields

--- Resources_class.py
+++ Resources_class.py
@@ -1,59 +1,54 @@
+from Resource_Class import Resource
+from Resource_Class import ResourceType
+
 class Resources:
     def __init__(self, metalValue = 0, crystalValue = 0, deuterValue = 0):
-        self.metal = metalValue
-        self.crystal = crystalValue
-        self.deuter = deuterValue
+        self.metal = Resource(ResourceType.Metal)
+        self.metal.setQuantity(metalValue)
 
-    def getMetal(self):
-        return self.metal
+        self.crystal = Resource(ResourceType.Crystal)
+        self.crystal.setQuantity(crystalValue)
 
-    def getCrystal(self):
-        return self.crystal
+        self.deuter = Resource(ResourceType.Deuter)
+        self.deuter.setQuantity(deuterValue)
 
-    def getDeuter(self):
-        return self.deuter
 
-    def setResources(self, metalValue = 0, crystalValue = 0, deuterValue = 0):
-        self.metal = metalValue
-        self.crystal = crystalValue
-        self.deuter = deuterValue
+    def getMetalValue(self):
+        return self.metal.getQuantity()
+
+    def getCrystalValue(self):
+        return self.crystal.getQuantity()
+
+    def getDeuterValue(self):
+        return self.deuter.getQuantity()
+
+    def setResources(self, metalValue, crystalValue , deuterValue):
+        self.metal.setQuantity(metalValue)
+        self.crystal.setQuantity(crystalValue)
+        self.deuter.setQuantity(deuterValue)
 
     def add(self, otherResourcesObject):
-        self.metal += otherResourcesObject.getMetal()
-        self.crystal += otherResourcesObject.getCrystal()
-        self.deuter += otherResourcesObject.getDeuter()
-
-    def addMetal(self, otherResourcesObject):
-        self.metal += otherResourcesObject.getMetal()
-
-    def addCrystal(self, otherResourcesObject):
-        self.metal += otherResourcesObject.getCrystal()
-
-    def addDeuter(self, otherResourcesObject):
-        self.metal += otherResourcesObject.getDeuter()
+        self.metal.add(otherResourcesObject.getMetal())
+        self.crystal.add(otherResourcesObject.getCrystal())
+        self.deuter.add(otherResourcesObject.getDeuter())
 
     def substract(self, otherResourcesObject):
-        self.metal -= otherResourcesObject.getMetal()
-        self.crystal -= otherResourcesObject.getCrystal()
-        self.deuter -= otherResourcesObject.getDeuter()
+        self.metal.substract(otherResourcesObject.getMetal())
+        self.crystal.substract(otherResourcesObject.getCrystal())
+        self.deuter.substract(otherResourcesObject.getDeuter())
 
-    def substractMetal(self, otherResourcesObject):
-        self.metal -= otherResourcesObject.getMetal()
+    def addResource(self, ResourceObject):
+        if ResourceObject.resourceType == ResourceType(1):
+            self.metal.add(ResourceObject)
+        elif ResourceObject.resourceType == ResourceType(2):
+            self.crystal.add(ResourceObject)
+        elif ResourceObject.resourceType == ResourceType(3):
+            self.deuter.add(ResourceObject)
 
-    def substractCrystal(self, otherResourcesObject):
-        self.metal -= otherResourcesObject.getCrystal()
-
-    def substractDeuter(self, otherResourcesObject):
-        self.metal -= otherResourcesObject.getDeuter()
-
-aaa = Resources(1000, 1000, 1000)
-bbb = Resources(1000, 1000, 1000)
-bbb.setResources(500, 500, 500)
-ccc = Resources()
-ccc.add(aaa)
-bbb.addCrystal(aaa)
-print(aaa.metal)
-print(bbb.crystal)
-print(ccc.deuter)
-ccc.substract(aaa)
-print(ccc.metal)
+    def substractResource(self, ResourceObject):
+        if ResourceObject.resourceType == ResourceType(1):
+            self.metal.substract(ResourceObject)
+        elif ResourceObject.resourceType == ResourceType(2):
+            self.crystal.substract(ResourceObject)
+        elif ResourceObject.resourceType == ResourceType(3):
+            self.deuter.substract(ResourceObject)
