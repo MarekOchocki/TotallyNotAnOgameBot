42e960676e1d5434f7a815dabf32b002e8161e38
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-13 14:58:40 +0200

    add Spaceship class file

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/Fleet/Spaceships.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallyNotAnOgameBot.Data.Fleet
+{
+    class Spaceships
+    {
+        public enum Type {LightFighter, HeavyFighter, Crusier, Battleship, Battlecrusier, Bomber, Destroyer, Deathstar};
+
+        private Type type;
+        private long quantity;
+
+        public Spaceships(Type spaceshipType, long spaceshipQuantity)
+        {
+            type = spaceshipType;
+            quantity = spaceshipQuantity;
+        }
+
+        public Type getType()
+        {
+            return type;
+        }
+
+        public long getQuantity()
+        {
+            return quantity;
+        }
+
+        public void setQuantity(long value)
+        {
+            quantity = value;
+        }
+
+        public void addQuantity(long value)
+        {
+            quantity += value;
+        }
+
+        public void substractQuantity(long value)
+        {
+            quantity -= value;
+        }
+    }
+}
