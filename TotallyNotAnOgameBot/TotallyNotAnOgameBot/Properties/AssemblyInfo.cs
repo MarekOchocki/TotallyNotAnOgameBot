b94412e3181ae42727c0739c1089d8b2ef46e921
MarekOchocki <marcopolo97@vp.pl>
2018-09-13 20:13:20 +0200

    add gitignore file

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie s¹ kontrolowane poprzez nastêpuj¹cy 
+// zestaw atrybutów. Zmieñ wartoœci tych atrybutów, aby zmodyfikowaæ informacje
+// powi¹zane z zestawem.
+[assembly: AssemblyTitle("TotallyNotAnOgameBot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TotallyNotAnOgameBot")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartoœæ false sprawia, ¿e typy w tym zestawie s¹ niewidoczne
+// dla sk³adników COM. Jeœli potrzebny jest dostêp do typu w tym zestawie z
+// COM, ustaw wartoœæ true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Nastêpuj¹cy identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostêpnienia tego projektu w modelu COM
+[assembly: Guid("36f9cc0f-2d31-4515-b8e4-8fc338df44d2")]
+
+// Informacje o wersji zestawu zawieraj¹ nastêpuj¹ce cztery wartoœci:
+//
+//      Wersja g³ówna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Mo¿esz okreœliæ wszystkie wartoœci lub u¿yæ domyœlnych numerów kompilacji i poprawki
+// przy u¿yciu symbolu „*”, tak jak pokazano poni¿ej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
