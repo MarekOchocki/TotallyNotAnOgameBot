863e14c96832889a9ce4a5d4cf9dd55c5f089fd2
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-13 14:57:37 +0200

    add Energy class file

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/Resource/Energy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallyNotAnOgameBot.Data.Resource
+{
+    class Energy
+    {
+        private long usage;
+        private long production;
+
+        public Energy(long energyUsage = 0, long energyProduction = 0)
+        {
+            usage = energyUsage;
+            production = energyProduction;
+        }
+
+        public long getUsage()
+        {
+            return usage;
+        }
+
+        public long getProduction()
+        {
+            return production;
+        }
+
+        public void setUsage(long value)
+        {
+            usage = value;
+        }
+
+        public void setProduction(int value)
+        {
+            production = value;
+        }
+
+        public long getEnergy()
+        {
+            return (getProduction() - getUsage());
+        }
+    }
+}
