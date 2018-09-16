60a6af5136e9fe0efb67701a050dec36a832437f
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-16 20:08:48 +0200

    add Resarch and ResarchState classes

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/Resarch/Resarch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallyNotAnOgameBot.Data.Resarch
+{
+    class Resarch
+    {
+        public enum Type {EnergyTechnology, LaserTechnology, IonTechnology, HyperspaceTechnology ,PlasmaTechnology, 
+        EspionageTechnology ,ComputerTechnology ,Astrophysics ,IntergalacticResearchNetwork , GravitonTechnology, 
+        WeaponsTechnology, ShieldingTechnology, ArmourTechnology, CombustionDrive, ImpulseDrive, HyperspaceDrive}
+
+        private int level;
+        private readonly Type type;
+
+        public Resarch(Type resarchType, int resarchLevel = 0)
+        {
+            type = resarchType;
+            level = resarchLevel;
+        }
+
+        public Type getType()
+        {
+            return type;
+        }
+
+        public int getLevel()
+        {
+            return level;
+        }
+
+        public void setLevel(int value)
+        {
+            level = value;
+        }
+
+        public void addLevel(int value)
+        {
+            level += value;
+        }
+
+        public void substractLevel(int value)
+        {
+            level -= value;
+        }
+    }
+}
---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/Resarch/ResarchState.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallyNotAnOgameBot.Data.Resarch
+{
+    class ResarchState
+    {
+        public Resarch EnergyTechnology;
+        public Resarch LaserTechnology;
+        public Resarch IonTechnology;
+        public Resarch HyperspaceTechnology;
+        public Resarch PlasmaTechnology;
+        public Resarch EspionageTechnology;
+        public Resarch ComputerTechnology;
+        public Resarch Astrophysics;
+        public Resarch IntergalacticResearchNetwork;
+        public Resarch GravitonTechnology;
+        public Resarch WeaponsTechnology;
+        public Resarch ShieldingTechnology;
+        public Resarch ArmourTechnology;
+        public Resarch CombustionDrive;
+        public Resarch ImpulseDrive;
+        public Resarch HyperspaceDrive;
+
+        public ResarchState()
+        {
+            EnergyTechnology = new Resarch(Resarch.Type.EnergyTechnology, 0);
+            LaserTechnology = new Resarch(Resarch.Type.LaserTechnology, 0);
+            IonTechnology = new Resarch(Resarch.Type.IonTechnology, 0);
+            HyperspaceTechnology = new Resarch(Resarch.Type.HyperspaceTechnology, 0);
+            PlasmaTechnology = new Resarch(Resarch.Type.PlasmaTechnology, 0);
+            EspionageTechnology = new Resarch(Resarch.Type.EspionageTechnology, 0);
+            ComputerTechnology = new Resarch(Resarch.Type.ComputerTechnology, 0);
+            Astrophysics = new Resarch(Resarch.Type.Astrophysics, 0);
+            IntergalacticResearchNetwork = new Resarch(Resarch.Type.IntergalacticResearchNetwork, 0);
+            GravitonTechnology = new Resarch(Resarch.Type.GravitonTechnology, 0);
+            WeaponsTechnology = new Resarch(Resarch.Type.WeaponsTechnology, 0);
+            ShieldingTechnology = new Resarch(Resarch.Type.ShieldingTechnology, 0);
+            ArmourTechnology = new Resarch(Resarch.Type.ArmourTechnology, 0);
+            CombustionDrive = new Resarch(Resarch.Type.CombustionDrive, 0);
+            ImpulseDrive = new Resarch(Resarch.Type.ImpulseDrive, 0);
+            HyperspaceDrive = new Resarch(Resarch.Type.HyperspaceDrive, 0);
+        }
+
+        public void add(ResarchState otherResarch)
+        {
+            EnergyTechnology.addLevel(otherResarch.EnergyTechnology.getLevel());
+            LaserTechnology.addLevel(otherResarch.LaserTechnology.getLevel());
+            IonTechnology.addLevel(otherResarch.IonTechnology.getLevel());
+            HyperspaceTechnology.addLevel(otherResarch.HyperspaceTechnology.getLevel());
+            PlasmaTechnology.addLevel(otherResarch.PlasmaTechnology.getLevel());
+            EspionageTechnology.addLevel(otherResarch.EspionageTechnology.getLevel());
+            ComputerTechnology.addLevel(otherResarch.ComputerTechnology.getLevel());
+            Astrophysics.addLevel(otherResarch.Astrophysics.getLevel());
+            IntergalacticResearchNetwork.addLevel(otherResarch.IntergalacticResearchNetwork.getLevel());
+            GravitonTechnology.addLevel(otherResarch.GravitonTechnology.getLevel());
+            WeaponsTechnology.addLevel(otherResarch.WeaponsTechnology.getLevel());
+            ShieldingTechnology.addLevel(otherResarch.ShieldingTechnology.getLevel());
+            ArmourTechnology.addLevel(otherResarch.ArmourTechnology.getLevel());
+            CombustionDrive.addLevel(otherResarch.CombustionDrive.getLevel());
+            ImpulseDrive.addLevel(otherResarch.ImpulseDrive.getLevel());
+            HyperspaceDrive.addLevel(otherResarch.HyperspaceDrive.getLevel());
+        }
+
+        public void substract(ResarchState otherResarch)
+        {
+            EnergyTechnology.substractLevel(otherResarch.EnergyTechnology.getLevel());
+            LaserTechnology.substractLevel(otherResarch.LaserTechnology.getLevel());
+            IonTechnology.substractLevel(otherResarch.IonTechnology.getLevel());
+            HyperspaceTechnology.substractLevel(otherResarch.HyperspaceTechnology.getLevel());
+            PlasmaTechnology.substractLevel(otherResarch.PlasmaTechnology.getLevel());
+            EspionageTechnology.substractLevel(otherResarch.EspionageTechnology.getLevel());
+            ComputerTechnology.substractLevel(otherResarch.ComputerTechnology.getLevel());
+            Astrophysics.substractLevel(otherResarch.Astrophysics.getLevel());
+            IntergalacticResearchNetwork.substractLevel(otherResarch.IntergalacticResearchNetwork.getLevel());
+            GravitonTechnology.substractLevel(otherResarch.GravitonTechnology.getLevel());
+            WeaponsTechnology.substractLevel(otherResarch.WeaponsTechnology.getLevel());
+            ShieldingTechnology.substractLevel(otherResarch.ShieldingTechnology.getLevel());
+            ArmourTechnology.substractLevel(otherResarch.ArmourTechnology.getLevel());
+            CombustionDrive.substractLevel(otherResarch.CombustionDrive.getLevel());
+            ImpulseDrive.substractLevel(otherResarch.ImpulseDrive.getLevel());
+            HyperspaceDrive.substractLevel(otherResarch.HyperspaceDrive.getLevel());
+        }
+    }
+}
