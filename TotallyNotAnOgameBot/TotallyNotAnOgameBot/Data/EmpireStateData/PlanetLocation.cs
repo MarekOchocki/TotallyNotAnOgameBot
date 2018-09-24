24064e4215c5bc759225405b563e91e693b3cb40
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-24 14:07:28 +0200

    add PlanetLocation class

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/EmpireStateData/PlanetLocation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallyNotAnOgameBot.Data.EmpireStateData
+{
+    class PlanetLocation
+    {
+        private readonly int planetNumber;
+        private readonly int galaxyNumber;
+        private readonly int systemNumber;
+
+
+        public PlanetLocation(int planetNumber, int systemNumber, int galaxyNumber)
+        {
+            this.planetNumber = planetNumber;
+            this.galaxyNumber = galaxyNumber;
+            this.systemNumber = systemNumber;
+        }
+
+        public int getPlanetNumber()
+        {
+            return planetNumber;
+        }
+
+        public int getGalaxyNumber()
+        {
+            return galaxyNumber;
+        }
+
+        public int getSystemNumber()
+        {
+            return systemNumber;
+        }
+    }
+}
