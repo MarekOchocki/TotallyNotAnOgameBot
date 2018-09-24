d8b4c93486f500d7b35370bc76f9c239c80b5244
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-24 20:09:40 +0200

    add changes after code review

--- TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/EmpireStateData/PlanetLocation.cs
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/EmpireStateData/PlanetLocation.cs
@@ -13,14 +13,14 @@ namespace TotallyNotAnOgameBot.Data.EmpireStateData
         public readonly int systemNumber;
 
 
-        public PlanetLocation(int planetNumber, int systemNumber, int galaxyNumber)
+        public PlanetLocation(int galaxyNumber, int systemNumber, int planetNumber)
         {
-            if ((planetNumber < 0) || (planetNumber > 15))
-                throw new ArgumentOutOfRangeException("less than 0 or more than 15");
-            if ((systemNumber < 0) || (systemNumber > 499))
-                throw new ArgumentOutOfRangeException("less than 0 or more than 499");
-            if ((galaxyNumber < 0) || (galaxyNumber > 7))
-                throw new ArgumentOutOfRangeException("less than 0 or more than 8");
+            if ((planetNumber < 1) || (planetNumber > 15))
+                throw new ArgumentOutOfRangeException("less than 1 or more than 15");
+            if ((systemNumber < 1) || (systemNumber > 499))
+                throw new ArgumentOutOfRangeException("less than 1 or more than 499");
+            if ((galaxyNumber < 1) || (galaxyNumber > 7))
+                throw new ArgumentOutOfRangeException("less than 1 or more than 7");
 
             this.planetNumber = planetNumber;
             this.galaxyNumber = galaxyNumber;
