977c9dcaf5933a4961695dc8109f2942e466eaa9
DESKTOP-EB1TGVK\kozumi <maniek123-45@o2.pl>
2018-09-25 14:36:15 +0200

    add TechRequirements class

---
+++ TotallyNotAnOgameBot/TotallyNotAnOgameBot/Data/TechnologyRequirements/TechRequirements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TotallyNotAnOgameBot.Data.Resarch;
+using TotallyNotAnOgameBot.Data.Buildings;
+
+namespace TotallyNotAnOgameBot.Data.TechnologyRequirements
+{
+    class TechRequirements
+    {
+        public ResarchState resarchState;
+        public StationBuildings stationBuildings;
+        public ProductionBuildings productionBuildings;
+        public MoonBuildings moonBuildings;
+
+        public TechRequirements()
+        {
+            resarchState = new ResarchState();
+            stationBuildings = new StationBuildings();
+            productionBuildings = new ProductionBuildings();
+            moonBuildings = new MoonBuildings();
+        }
+
+    }
+}
